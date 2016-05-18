0b264c0faa4ab30bc0dfbcdaa5d2298c5cb7c65d
Neuroticcheeze <neuroticcheeze@Hotmail.com>
2016-05-18 22:16:12 +1000

    Added texture asset obj

---
+++ Meridian 2D Game Engine/Src/Engine/Assets/AssetTexture.cpp
@@ -0,0 +1,84 @@
+/*===================================================================
+//				(Copyright, 2016) Jesse Farag.
+//	Distributed under the Meridian Technologies License, Version 1.0.
+//			(See included file MERIDIAN_LICENSE.txt)
+-------------------------------------------------------------------
+//
+//Source File:	AssetTexture.cpp
+//
+===================================================================*/
+
+#include "AssetTexture.hpp"
+#include "..\Core\Meridian.hpp"
+
+using namespace Meridian;
+
+AssetTexture::AssetTexture()
+{
+}
+
+AssetTexture::~AssetTexture()
+{
+
+}
+
+#ifdef _DEBUG
+
+void AssetTexture::Load(const char ** p_resourcePaths)
+{
+	const char * path = p_resourcePaths[0];
+
+
+}
+
+void AssetTexture::Encode(SerialBuffer & p_buffer) const
+{
+	int reqSize = 
+		sizeof(m_width) +					//Width
+		sizeof(m_height) +					//Height
+		sizeof(m_channels) +				//Number of colour channels
+		m_channels * m_width * m_height;	//Pixel data
+
+	//Set buffer with size needed to store this texture.
+	p_buffer.Reallocate(reqSize);
+
+	memcpy_s(p_buffer.Data(0), sizeof(m_width), &m_width, sizeof(m_width));
+	memcpy_s(p_buffer.Data(sizeof(m_width)), sizeof(m_height), &m_height, sizeof(m_height));
+	memcpy_s(p_buffer.Data(sizeof(m_width) + sizeof(m_height)), sizeof(m_channels), &m_channels, sizeof(m_channels));
+}
+
+#endif
+
+void AssetTexture::Decode(const SerialBuffer & p_buffer)
+{
+	int reqSize =
+		sizeof(m_width) +					//Width
+		sizeof(m_height) +					//Height
+		sizeof(m_channels) +				//Number of colour channels
+		m_channels * m_width * m_height;	//Pixel data
+	
+	assert(p_buffer.Size() == reqSize);
+
+	memcpy_s(&m_width, sizeof(m_width), p_buffer.C_Data(0), sizeof(m_width));
+	memcpy_s(&m_height, sizeof(m_height), p_buffer.C_Data(sizeof(m_width)), sizeof(m_height));
+	memcpy_s(&m_channels, sizeof(m_channels), p_buffer.C_Data(sizeof(m_width) + sizeof(m_height)), sizeof(m_channels));
+}
+
+SerialBuffer::SerialBuffer() :
+	m_buffer(nullptr),
+	m_size(0)
+{
+
+}
+
+SerialBuffer::SerialBuffer(const SerialBuffer & p_other)
+{
+	Reallocate(p_other.m_size);
+	memcpy_s(m_buffer, m_size, p_other.m_buffer, m_size);
+}
+
+SerialBuffer::~SerialBuffer()
+{
+	free(m_buffer);
+	m_buffer = nullptr;
+}
---
+++ Meridian 2D Game Engine/Src/Engine/Assets/AssetTexture.hpp
@@ -0,0 +1,58 @@
+/*===================================================================
+//				(Copyright, 2016) Jesse Farag.
+//	Distributed under the Meridian Technologies License, Version 1.0.
+//			(See included file MERIDIAN_LICENSE.txt)
+-------------------------------------------------------------------
+//
+//Header File:	AssetTexture.hpp
+//
+//Objects:		AssetTexture
+//					-Implements IAsset
+//
+===================================================================*/
+
+//TODO: Add interface with mouse and keyboard. Eventually for handheld devices, etc.
+
+#pragma once
+
+#include "..\Core\Asset.hpp"
+
+namespace Meridian
+{
+	/*=============================================================================
+	Asset for textures that can be drawn with versatile properties such as position,
+	scales, rotation, tint, transparency, etc. Or used in many other ways.
+	=============================================================================*/
+	class AssetTexture : public IAsset
+	{
+	public:///Friendly Internal Forward Declarations
+
+		friend class ResourceManager;
+
+	private:///Constructors/Destructors
+
+		AssetTexture();
+		~AssetTexture();
+
+#ifdef _DEBUG
+		/*Used in debug mode to read in raw resources (.png, .xml, .mp3, etc), and fill this
+		asset's members just like Decode does (instead from the resource file). Once a resource
+		file has been generated during development, this asset will get its data from there instead.*/
+		void Load(const char ** p_resourcePaths);
+
+		/*Used in debug mode to serialise its data into a buffer to be written to the game's
+		single resource file to be read in release build by Decode. Since the resource file is
+		never changed in release mode, Encode is not needed.*/
+		void Encode(SerialBuffer & p_buffer) const;
+#endif
+		/*This function is always used to read from the resource file when the game starts up.*/
+		void Decode(const SerialBuffer & p_buffer);
+		
+	private:///Member Fields
+
+		unsigned short m_width;
+		unsigned short m_height;
+		byte m_channels;
+
+	};
+}
