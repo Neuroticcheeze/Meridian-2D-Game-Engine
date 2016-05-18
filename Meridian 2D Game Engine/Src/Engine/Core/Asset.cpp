a111125de31e86f2dfab6fed9a8c6f805aeb1367
Neuroticcheeze <neuroticcheeze@Hotmail.com>
2016-05-18 21:35:40 +1000

    Added asset base class

--- Meridian 2D Game Engine/Src/Engine/Core/Asset.cpp
+++ Meridian 2D Game Engine/Src/Engine/Core/Asset.cpp
@@ -8,51 +8,16 @@
 //
 ===================================================================*/
 
-#include "Meridian.hpp"
+#include "Asset.hpp"
 
-namespace Meridian
+using namespace Meridian;
+
+IAsset::IAsset()
 {
-	//External Forward Declarations
-	struct SerialBuffer;
 
-	/*========================================================================
-	Base structure for an intermediate object between the game and saved data.
-	==========================================================================*/
-	class IAsset
-	{
-	public:///Friendly Internal Forward Declarations
+}
 
-		friend class ResourceManager;
+IAsset::~IAsset()
+{
 
-	protected:///Constructor/Destructor
-
-		IAsset();
-		virtual ~IAsset();
-
-	protected:///Member Functions
-
-#ifdef _DEBUG
-			  /*Used in debug mode to read in raw resources (.png, .xml, .mp3, etc), and fill this
-			  asset's members just like Decode does (instead from the resource file). Once a resource
-			  file has been generated during development, this asset will get its data from there instead.*/
-		virtual void Load(const char ** p_resourcePaths) = 0;
-
-		/*Used in debug mode to serialise its data into a buffer to be written to the game's
-		single resource file to be read in release build by Decode. Since the resource file is
-		never changed in release mode, Encode is not needed.*/
-		virtual void Encode(SerialBuffer & p_buffer) const = 0;
-#endif
-		/*This function is always used to read from the resource file when the game starts up.*/
-		virtual void Decode(const SerialBuffer & p_buffer) = 0;
-	};
-
-
-	/*==================================================================
-	An object to store a stream of bytes and how many bytes it contains.
-	==================================================================*/
-	struct SerialBuffer
-	{
-		byte * buffer;
-		unsigned int size;
-	};
 }
