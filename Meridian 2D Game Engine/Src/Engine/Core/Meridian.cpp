2f0488debe13d9238370173c6a671e3afe880313
Neuroticcheeze <neuroticcheeze@Hotmail.com>
2016-05-18 17:05:59 +1000

    Fixed up some comments. More tiny tweaks

--- Meridian 2D Game Engine/Src/Engine/Core/Meridian.cpp
+++ Meridian 2D Game Engine/Src/Engine/Core/Meridian.cpp
@@ -29,6 +29,7 @@ MeridianEngine::MeridianEngine() :
 
 MeridianEngine::~MeridianEngine()
 {
+	//Delete any modules and clear the list to release memory when this engine gets deleted.
 	for (auto module : m_modules)
 	{
 		delete module;
@@ -43,16 +44,23 @@ void MeridianEngine::Load()
 	{
 		m_isInitialised = true;
 
+		//If the window was not successfully created, unload the engine. Since the game loop hasn't been hit yet, unload runs straight away.
 		if ((m_window = glfwCreateWindow(640, 480, "Meridian-2D Game", nullptr, nullptr)) == nullptr)
-			Terminate();
+		{
+			Unload();
+			return;
+		}
 	}
 
 	glfwMakeContextCurrent(m_window);
 
 	if (ogl_LoadFunctions() == ogl_LOAD_FAILED)
-		Terminate();
-	
-	return;
+	{
+		Unload();
+		return;
+	}
+
+	//Since by this point, we've successfully started everything up, we can now initialise all attached modules.
 
 	for (auto module : m_modules)
 	{
@@ -89,17 +97,20 @@ void MeridianEngine::Run(const GameLoopMode & p_mode)
 	float m_startTime = 0, m_finishTime = 0;
 	float m_remainder = 0, m_duration = 0, m_deltaTime = 0;
 
-	while (m_isRunning)
+	while (m_isRunning && !glfwWindowShouldClose(m_window))
 	{
+		glfwPollEvents();
+
 		m_startTime = (float)glfwGetTime();
 
-		//Loop
-		printf("Capped FPS: %f, True FPS: %f\n", min(1.0F / m_duration, m_targetFramerate), 1.0F / m_duration);
+		//===============Loop===============
+		///printf("Capped FPS: %f, True FPS: %f\n", min(1.0F / m_duration, m_targetFramerate), 1.0F / m_duration);
 		for (auto module : m_modules)
 		{
 			module->Update(this, m_deltaTime);
 			module->Render(this);
 		}
+		//==================================
 
 		m_finishTime = (float)glfwGetTime();
 
@@ -121,21 +132,27 @@ void MeridianEngine::Run(const GameLoopMode & p_mode)
 
 void MeridianEngine::Unload()
 {
+	//If we're not initialised, then we cannot finalise anything.
+	if (!m_isInitialised)
+		return;
+
+
+	//Finalise all attached modules.
 	for (auto module : m_modules)
 	{
 		module->Finalise(this);
 	}
 
-	if (m_isInitialised)
-	{
-		glfwDestroyWindow(m_window);
-		glfwTerminate();
-	}
+	//Kill the window and terminate glfw.
+	glfwDestroyWindow(m_window);
+	glfwTerminate();
 
+	//No longer initialised since we've undone everything initialise did.
 	m_isInitialised = false;
 }
 
 void MeridianEngine::Terminate()
 {
+	//Kill the game loop by the next iteration.
 	m_isRunning = false;
 }
